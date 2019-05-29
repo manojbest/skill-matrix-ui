5c841a5513065215ea3abbc6c792c203e39aa4f6
Bandara <mbandara@wiley.com>
2019-05-29 15:32:23 +0530

    removed files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Container>
+          <Row>
+              <Col md={3}>Wiley</Col>
+              <Col md={5}>Skill Matrix</Col>
+              <Col md={2}>C</Col>
+          </Row>
+          <Row>
+              <Col>D</Col>
+              <Col>E</Col>
+          </Row>
+        </Container>
     </div>
   );
 }
